2938ecc92c9357a88193a76ee7c04c3d5f150847
Dakota Hoskins <dakotaahoskinss@gmail.com>
2019-08-07 18:51:09 -0400

    forms render successfully

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import FormComponent from './components/FormComponent';
+
 function App() {
   return (
     <div className="App">
-
+      <FormComponent />
     </div>
   );
 }
